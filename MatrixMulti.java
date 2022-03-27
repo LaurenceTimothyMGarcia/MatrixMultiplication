c19832a87055e3c4fd2e1b4f982621e490841ff9
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2022-03-27 08:59:40 -0700

    Set up build matrix function
    Set up build matrix function to build a matrix by random numbers

--- MatrixMulti.java
+++ MatrixMulti.java
@@ -1,12 +1,39 @@
+import java.lang.reflect.Array;
+
 //Laurence Timothy M. Garcia
 //CS 3310.01
 //Professor Huong Luu
 //Project 1 - Matrix Multiplication
 
+import java.util.Random;
+
 public class MatrixMulti
 {
     public static void main(String[] args)
     {
-        
+        int[][] matrix1 = new int[4][4];
+        int[][] matrix2 = new int[4][4];
+
+        int[][] matrixProduct;
+
+        matrix1 = buildMatrix(matrix1);
+        matrix2 = buildMatrix(matrix2);
+    }
+
+    public int[][] buildMatrix(int[][] matrix)
+    {
+        Random rand = new Random();
+
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                matrix[i][j] = rand.nextInt(10);
+            }
+        }
+
+        System.out.print(matrix);
+
+        return matrix;
     }
 }
