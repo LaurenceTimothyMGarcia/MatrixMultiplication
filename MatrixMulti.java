a41db590b6a4415563df3378bfa12eda672c195e
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2022-03-27 16:34:24 -0700

    implemented strassen, add, split
    Fixed up slit and strassen, still working on add, and going to add join in a bit

--- MatrixMulti.java
+++ MatrixMulti.java
@@ -23,6 +23,11 @@ public class MatrixMulti
 
         System.out.println("Matrix 2");
         printMatrix(matrix2);
+
+        matrixProduct = strassen(matrix1, matrix2);
+
+        System.out.println("Product of Matrix 1 and Matrix 2");
+        printMatrix(matrixProduct);
     }
 
     /*** Builds Random Matrix ***/
@@ -58,7 +63,7 @@ public class MatrixMulti
     }
 
     /*** Strassen's Algorithm ***/
-    public int[][] strassenSplit(int[][] mat)
+    public static int[][][] strassenSplit(int[][] mat)
     {
         int row = mat.length;
         int col = mat.length;
@@ -66,23 +71,91 @@ public class MatrixMulti
         int rowDiv = row/2;
         int colDiv = col/2;
 
-        int[][] mat1 = new int[rowDiv][colDiv];
+        int[][] newMatA = new int[rowDiv][colDiv];
+        int[][] newMatB = new int[rowDiv][colDiv];
+        int[][] newMatC = new int[rowDiv][colDiv];
+        int[][] newMatD = new int[rowDiv][colDiv];
 
-        for (int i = 0; i < mat.length; i++)
+        for (int i = 0; i < rowDiv; i++)
         {
-            
+            for (int j = 0; j < colDiv; j++)
+            {
+                newMatA[i][j] = mat[i][j];                  //Top left quadrant
+                newMatB[i][j] = mat[i][j + colDiv];         //Top right quadrant
+                newMatC[i][j] = mat[i + rowDiv][j];         //Bottom left quadrant
+                newMatD[i][j] = mat[i + rowDiv][j + colDiv];//Bottom right quadrant
+            }
         }
 
-        return mat1;
+        int[][][] matArray = {newMatA, newMatB, newMatC, newMatD};
+
+        return matArray;
     }
 
-    public int[][] strassenDiv(int[][] matA, int[][] matB)
+    //Adding the product of 2 matricies together
+    public static int[][] strassenAdd(int[][] matA, int[][] matB)
     {
-        if (matA.length <= 2)
+        int matSize = matA.length;
+        int[][] matSum = new int[matSize][matSize];
+
+        for (int i = 0; i < matSize; i++)
         {
-            
+            for (int j = 0; j < matSize; j++)
+            {
+                matSum[i][j] = matA[i][j] + matB[i][j];
+            }
         }
 
+        return matSum;
+    }
 
+    public static int[][] strassen(int[][] matA, int[][] matB)
+    {
+        int matSize = matA.length;
+
+        int[][] matC = new int[matSize][matSize];
+
+        if (matSize <= 1)
+        {
+            matC[0][0] = matA[0][0] * matB[0][0];
+            return matC;
+        }
+
+        //Splits the matrix into quadrants and returns as 3d array
+        int[][][] matASplit = strassenSplit(matA);
+        int[][][] matBSplit = strassenSplit(matB);
+
+        //Takes the array and places into matrix variables
+        int[][] matA1 = matASplit[0];
+        int[][] matA2 = matASplit[1];
+        int[][] matA3 = matASplit[2];
+        int[][] matA4 = matASplit[3];
+
+        int[][] matB1 = matBSplit[0];
+        int[][] matB2 = matBSplit[1];
+        int[][] matB3 = matBSplit[2];
+        int[][] matB4 = matBSplit[3];
+
+        //Recursively calling Strassen to continue breaking down the problem to 1x1 matrix
+        int[][] matA1B1 = strassen(matA1, matB1);
+        int[][] matA2B3 = strassen(matA2, matB3);
+
+        int[][] matA1B2 = strassen(matA1, matB2);
+        int[][] matA2B4 = strassen(matA2, matB4);
+
+        int[][] matA3B1 = strassen(matA3, matB1);
+        int[][] matA4B3 = strassen(matA4, matB3);
+
+        int[][] matA3B2 = strassen(matA3, matB2);
+        int[][] matA4B4 = strassen(matA4, matB4);
+
+        //Adding the products together
+        int[][] matC11 = strassenAdd(matA1B1, matA2B3);
+        int[][] matC12 = strassenAdd(matA1B2, matA2B4);
+        int[][] matC21 = strassenAdd(matA3B1, matA4B3);
+        int[][] matC22 = strassenAdd(matA3B2, matA4B4);
+
+
+        return matC;
     }
 }
