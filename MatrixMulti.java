1a57945b073d4e7baa16750507566eb5e02d3858
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2022-03-26 23:09:46 -0700

    added main class
    main class and main function was added

---
+++ MatrixMulti.java
@@ -0,0 +1,12 @@
+//Laurence Timothy M. Garcia
+//CS 3310.01
+//Professor Huong Luu
+//Project 1 - Matrix Multiplication
+
+public class MatrixMulti
+{
+    public static void main(String[] args)
+    {
+        
+    }
+}
